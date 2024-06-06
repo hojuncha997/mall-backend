3c45a094119eeb1e9382ef225888cd9209b3f776
hojuncha997 <hojun.cha997@gmail.com>
2024-06-06 18:40:33 +0900

    feat: @ResControllerAdvice 추가

---
+++ src/main/java/com/test/mallapi/controller/advice/CustomControllerAdvice.java
@@ -0,0 +1,36 @@
+package com.test.mallapi.controller.advice;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+/*
+* CustomControllerAdvice
+* */
+
+// @RestControllerAdvice가 적용되면 예외가 발생해도 호출한 쪽으로 HTTP 상태 코드와 JSON 메시지를 전달할 수 있다.
+
+@RestControllerAdvice
+public class CustomControllerAdvice {
+
+    @ExceptionHandler(NoSuchElementException.class)
+    protected ResponseEntity<?> noExist(NoSuchElementException e) {
+        String msg = e.getMessage();
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("msg", msg));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<?> handleIllegalArgumentException(MethodArgumentNotValidException e) {
+        String msg = e.getMessage();
+
+        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(Map.of("msg", msg));
+    }
+
+}
