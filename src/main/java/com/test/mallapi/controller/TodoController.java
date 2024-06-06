dbe448c6ef6c9861f4703b3ab048153db773a20e
hojuncha997 <hojun.cha997@gmail.com>
2024-06-06 19:19:27 +0900

    ㄹfeat: ìtodoController에 POST, PUT, DELETE API 추가

--- src/main/java/com/test/mallapi/controller/TodoController.java
+++ src/main/java/com/test/mallapi/controller/TodoController.java
@@ -8,10 +8,9 @@ import jakarta.annotation.Resource;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -32,5 +31,39 @@ public class TodoController {
 
         return todoService.list(pageRequestDTO);
     }
+
+    @PostMapping("/")
+    public Map<String, Long> register(@RequestBody TodoDTO todoDTO) {
+        log.info("TodoDTO: " + todoDTO);
+        Long tno = todoService.register(todoDTO);
+        return Map.of("TNO", tno);
+    }
+
+    @PutMapping("/{tno}")
+    public Map<String, String> modify(
+            @PathVariable(name = "tno") Long tno,
+            @RequestBody TodoDTO todoDTO) {
+        todoDTO.setTno(tno);
+        log.info("Modify: " + todoDTO);
+
+        todoService.modify(todoDTO);
+        return Map.of("RESULT", "SUCCESS");
+    }
+
+    @DeleteMapping("/{tno}")
+    public Map<String, String> remove(@PathVariable(name = "tno") Long tno) {
+        log.info("Remove: " + tno);
+        todoService.remove(tno);
+
+        return Map.of("RESULT", "SUCCESS");
+        /*
+        DELETE의 경우에는 POST/PUT과 달리 전달하는 payload가 제한적이다. URL에 사용되는 특문을 사용하기 위해서는 URL인코딩 처리를 해줘야 한다.
+        공백문자나 특문이 포함된 데이터는 정상적으로 처리되지 않기 때문에 주의 필요
+        */
+    }
+
+
+
+
 
 }
