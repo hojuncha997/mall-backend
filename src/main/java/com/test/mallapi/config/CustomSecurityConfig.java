f498a5b3b022d9b2a0e494021792ee3b2e999867
hjcha <hjcha@markany.com>
2024-06-17 16:24:08 +0900

    feat: Spring security 기본설정

---
+++ src/main/java/com/test/mallapi/config/CustomSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.test.mallapi.config;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@Log4j2
+@RequiredArgsConstructor
+public class CustomSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        log.info("-------------------------Security Config-------------------------");
+
+        return http.build();
+
+    }
+}
+
+
+/*
+* 스프링 시큐리티와 관련된 설정을 위한 파일
+*
+* */
