1edbed970ea481ba56eabf1e6571dcce9beab6ff
hjcha <hjcha@markany.com>
2024-06-18 08:52:13 +0900

    feat: member 엔티티와 memberRole 생성 및 테스트

---
+++ src/main/java/com/test/mallapi/domain/MemberRole.java
@@ -0,0 +1,6 @@
+// 회원권한. Member에서 @ElementCollection으로 처리
+package com.test.mallapi.domain;
+
+public enum MemberRole {
+    USER, MANAGER, ADMIN
+}
---
+++ src/main/java/com/test/mallapi/repository/MemberRepository.java
@@ -0,0 +1,16 @@
+package com.test.mallapi.repository;
+
+import com.test.mallapi.domain.Member;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+
+public interface MemberRepository extends JpaRepository<Member, String> {
+    
+    //  조회 시에 권한의 목록까지 같이 로딩하도록 설정
+    @EntityGraph(attributePaths = {"memberRoleList"})
+    @Query("select m from Member m where m.email = :email")
+    Member getWithRoles(@Param("email") String email);
+}
